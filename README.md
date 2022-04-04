e046890e88b593028f985b139b506a29751e0626
fib <jshaferfam@protonmail.com>
2022-04-04 11:31:31 -0500

    comment out threading not in use. add to readme

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # bAuthBrute
 basic auth endpoint brute forcer
+
+# usage
+go run main.go -users userListLocation -pass passwordListLocation -u https://site.com/thing
+
+# TODO
+* add some ability to slow this down, currently it's gonna get you ratelimited quickly
+* add ability to add cookies in case these are needed to hit endpoint
--- main.go
+++ main.go
@@ -11,12 +11,13 @@ func main() {
 	//====================	Handle flags
 	userList := flag.String("users", "./sampleUsers", "user list")
 	passwordList := flag.String("pass", "./small", "password list")
-	threads := flag.Int("t", 1, "threads")
+	//====================	Threading TBD - probably need to figure out a way to slow this down
+	// threads := flag.Int("t", 1, "threads")
 	urlInput := flag.String("u", "http://localhost:8080/test", "target url")
 	//================TODO	probably need a cookie(s) value here too
 
 	flag.Parse()
-	fmt.Println(*threads)
+	// fmt.Println(*threads)
 	//====================	Handle Files
 	users := fileHandler.ImportFile(*userList)
 	pass := fileHandler.ImportFile(*passwordList)
